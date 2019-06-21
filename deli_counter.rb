2d49b28734db7f591a48e980f9c5411cdeb8f015
Rene Cocom <fasnimo@gmail.com>
2019-06-21 02:53:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,15 @@
 def line(array)
+  if array.length >= 1
   newarray = []
   number = 1
   array.each do |name|
-    if array.length >= 1
-        puts "The line is currently: #{newarray.join(" ")}"
-newarray << ("#{number}. #{name}")
-number += 1
+  newarray << ("#{number}. #{name}")
+  number += 1
+  end
+  puts "The line is currently: #{newarray.join(" ")}"
+
+  else
+      puts "The line is currently empty."
 
 end
 end
