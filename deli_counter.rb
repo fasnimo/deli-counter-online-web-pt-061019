e32c3abad6e6a9e6c03daad0b33787242fa5126c
Rene Cocom <fasnimo@gmail.com>
2019-06-21 03:13:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,8 +23,7 @@ end
 def now_serving(array)
   if array = []
 puts "There is nobody waiting to be served!"
-else
-puts "Currently serving #{array[0]}."
+else puts "Currently serving #{array[0]}."
 array.shift
 end
 end
